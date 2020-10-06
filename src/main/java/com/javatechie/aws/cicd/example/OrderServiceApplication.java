c7e8fc81bc7323fcdaa66d8aaecc8b6233c24a0b
suresh <srs7679@gmail.com>
2020-10-06 21:58:51 +0530

    checking backend

--- src/main/java/com/javatechie/aws/cicd/example/OrderServiceApplication.java
+++ src/main/java/com/javatechie/aws/cicd/example/OrderServiceApplication.java
@@ -1,25 +1,32 @@
 package com.javatechie.aws.cicd.example;
 
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Comparator;
-import java.util.List;
-import java.util.stream.Collectors;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 
 @SpringBootApplication
 @RestController
 @RequestMapping("/orders")
 public class OrderServiceApplication {
 
-    @Autowired
-    private OrderDao orderDao;
+	@Autowired
+	private OrderDao orderDao;
 
-    @GetMapping
+	@GetMapping
     public List<Order> fetchOrders() {
         return orderDao.getOrders().stream().
                 sorted(Comparator.comparing(Order::getPrice)).collect(Collectors.toList());
